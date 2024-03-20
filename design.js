9314f7cec01ed991dca7b867de2387b756ed7d2a
saisharandeep <137923510+saisharandeep@users.noreply.github.com>
2024-03-20 11:57:38 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,23 @@
+function addition(){
+    var a= parseInt(document.getElementById("n1").value);
+    var b= parseInt(document.getElementById("n2").value);
+    document.getElementById("res").textContent=a+b;
+}
+function subtraction(){
+    var a=parseInt(document.getElementById("n1").value);
+    var b=parseInt(document.getElementById("n2").value);
+    document.getElementById("res").textContent=a-b;
+
+}
+function multiplication(){
+    var a=parseInt(document.getElementById("n1").value);
+    var b=parseInt(document.getElementById("n2").value);
+    document.getElementById("res").textContent=a*b;
+
+}
+function divide(){
+    var a=parseInt(document.getElementById("n1").value);
+    var b=parseInt(document.getElementById("n2").value);
+    document.getElementById("res").textContent=a/b;
+
+}
